0ec51e19dc1b9d82c46b80a738bdebf9ff94720d
Damon P <damon@inxaos.com>
2023-10-16 21:14:54 -0700

    Force set

--- components/remote_transmitter/remote_transmitter_esp32.cpp
+++ components/remote_transmitter/remote_transmitter_esp32.cpp
@@ -32,7 +32,8 @@ void RemoteTransmitterComponent::dump_config() {
 void RemoteTransmitterComponent::configure_rmt_() {
   rmt_config_t c{};
 
-  this->channel_ = override_rmt_channel;
+  //this->channel_ = override_rmt_channel;
+  this->channel_ = 0;
   this->config_rmt(c);
   c.rmt_mode = RMT_MODE_TX;
   c.gpio_num = gpio_num_t(this->pin_->get_pin());
